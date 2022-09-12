51d3a7e240d0be9a4d81384ebb6a1120ddf40e2f
danielprwn <danielprwn@gmail.com>
2022-09-12 12:13:39 +0200

    add files

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
+import Header from './components/Header/Header';
+import Robots from './containers/Robots/Robots';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
+const AppWrapper = styled.div`
+  text-align: center;
+  width: 100%;
+`;
+class App extends Component {
+  render() {
+    return (
+      <>
+        <GlobalStyle />
+        <AppWrapper>
+          <Header />
+          <Robots/>
+        </AppWrapper>
+      </>
+    );
+  }
+}
+export default App;
